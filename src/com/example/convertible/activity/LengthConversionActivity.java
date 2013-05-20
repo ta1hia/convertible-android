288be5cedb3da2333860323229f296e4aefa8509
Tahia Khan <tahia.khan@gmail.com>
2013-05-19 20:52:27 -0400

    trying spinner

--- src/com/example/convertible/activity/LengthConversionActivity.java
+++ src/com/example/convertible/activity/LengthConversionActivity.java
@@ -3,11 +3,15 @@ package com.example.convertible.activity;
 import android.app.Activity;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 import com.example.convertible.R;
 
 
 public class LengthConversionActivity extends Activity {
+
+    private Spinner mLengthSpinner;
 
     /**
      * Called when the activity is first created.
@@ -35,6 +39,22 @@ public class LengthConversionActivity extends Activity {
         txt = (TextView) findViewById(R.id.result_amount_unit);
         txt.setTypeface(font2);
 
+        initLengthFromSpinner();
+        initLengthToSpinner();
 
+    }
+
+    private void initLengthFromSpinner() {
+        mLengthSpinner = (Spinner) findViewById(R.id.length_from_spinner);
+        ArrayAdapter<CharSequence> lengthAdapter = ArrayAdapter.createFromResource(this, R.array.length_units_array, R.layout.simple_spinner_item);
+        lengthAdapter.setDropDownViewResource(R.layout.simple_spinner_dropdown_item);
+        mLengthSpinner.setAdapter(lengthAdapter);
+    }
+
+    private void initLengthToSpinner() {
+        mLengthSpinner = (Spinner) findViewById(R.id.length_to_spinner);
+        ArrayAdapter<CharSequence> lengthAdapter = ArrayAdapter.createFromResource(this, R.array.length_units_array, R.layout.simple_spinner_item);
+        lengthAdapter.setDropDownViewResource(R.layout.simple_spinner_dropdown_item);
+        mLengthSpinner.setAdapter(lengthAdapter);
     }
 }
